4b271863e5c84a36a567a8d05eb043812deec768
Andrew Palmer <andrew.pa@outlook.com>
2017-07-21 14:30:14 -0700

    finish DPI awareness so far

--- src/windows/mod.rs
+++ src/windows/mod.rs
@@ -9,8 +9,7 @@ pub struct RenderContext {
 }
 
 impl RenderContext {
-    fn new(nwin: &vgu::Window) -> Result<RenderContext, Box<Error>> {
-        let d2fac = vgu::Factory::new()?;
+    fn new(d2fac: vgu::Factory, nwin: &vgu::Window) -> Result<RenderContext, Box<Error>> {
         let rt = vgu::WindowRenderTarget::new(d2fac.clone(), &nwin)?;
         let scb = vgu::Brush::solid_color(rt.clone(), vgu::D2D1_COLOR_F{r:0.0,g:0.0,b:0.0,a:1.0})?;
         Ok(RenderContext { d2fac, rt, scb })
@@ -42,6 +41,7 @@ impl RenderContext {
                              self.scb.p, stroke_width, std::ptr::null_mut());
         }
     }
+
 }
 
 type NativeWindow = vgu::Window;
@@ -55,6 +55,11 @@ pub struct Window<'app> {
 impl Point {
     fn from_lparam(l: vgu::LPARAM) -> Point {
         Point { x: vgu::GET_X_LPARAM(l) as f32, y: vgu::GET_Y_LPARAM(l) as f32 }
+    }
+    pub fn to_dip(&self, rx: &mut RenderContext) -> Point {
+        let mut dpi: (f32, f32) = (0.0, 0.0);
+        unsafe { rx.rt.GetDpi(&mut dpi.0, &mut dpi.1); }
+        Point { x: (self.x * 96.0) / dpi.0, y: (self.y * 96.0) / dpi.1 }
     }
 }
 
@@ -79,19 +84,19 @@ unsafe extern "system" fn global_winproc(win: vgu::HWND, msg: vgu::UINT, w: vgu:
             0
         },
         vgu::WM_MOUSEMOVE => {
-            rwin.app.event(Event::MouseMove(Point::from_lparam(l), 
+            rwin.app.event(Event::MouseMove(Point::from_lparam(l).to_dip(&mut rwin.rx), 
                                             if w & 0x0001 != 0 { Some(MouseButton::Left) }
                                             else if w & 0x0002 != 0 { Some(MouseButton::Right) }
                                             else if w & 0x0010 != 0 { Some(MouseButton::Middle) }
                                             else { None }));
             0
         },
-        vgu::WM_LBUTTONDOWN => { rwin.app.event(Event::MouseDown(Point::from_lparam(l), MouseButton::Left)); 0 },
-        vgu::WM_LBUTTONUP => { rwin.app.event(Event::MouseUp(Point::from_lparam(l), MouseButton::Left)); 0 },
-        vgu::WM_RBUTTONDOWN => { rwin.app.event(Event::MouseDown(Point::from_lparam(l), MouseButton::Right)); 0 },
-        vgu::WM_RBUTTONUP => { rwin.app.event(Event::MouseUp(Point::from_lparam(l), MouseButton::Right)); 0 },
-        vgu::WM_MBUTTONDOWN => { rwin.app.event(Event::MouseDown(Point::from_lparam(l), MouseButton::Middle)); 0 },
-        vgu::WM_MBUTTONUP => { rwin.app.event(Event::MouseUp(Point::from_lparam(l), MouseButton::Middle)); 0 },
+        vgu::WM_LBUTTONDOWN => { rwin.app.event(Event::MouseDown(Point::from_lparam(l).to_dip(&mut rwin.rx), MouseButton::Left)); 0 },
+        vgu::WM_LBUTTONUP => { rwin.app.event(Event::MouseUp(Point::from_lparam(l).to_dip(&mut rwin.rx), MouseButton::Left)); 0 },
+        vgu::WM_RBUTTONDOWN => { rwin.app.event(Event::MouseDown(Point::from_lparam(l).to_dip(&mut rwin.rx), MouseButton::Right)); 0 },
+        vgu::WM_RBUTTONUP => { rwin.app.event(Event::MouseUp(Point::from_lparam(l).to_dip(&mut rwin.rx), MouseButton::Right)); 0 },
+        vgu::WM_MBUTTONDOWN => { rwin.app.event(Event::MouseDown(Point::from_lparam(l).to_dip(&mut rwin.rx), MouseButton::Middle)); 0 },
+        vgu::WM_MBUTTONUP => { rwin.app.event(Event::MouseUp(Point::from_lparam(l).to_dip(&mut rwin.rx), MouseButton::Middle)); 0 },
         vgu::WM_DESTROY => { vgu::PostQuitMessage(0); 1 }
         _ => { vgu::DefWindowProcW(win, msg, w, l) }
     }
@@ -100,8 +105,13 @@ unsafe extern "system" fn global_winproc(win: vgu::HWND, msg: vgu::UINT, w: vgu:
 impl<'app> Window<'app> {
     pub fn new(title: &str, width: usize, height: usize, app: &'app mut App) -> Result<Self, Box<Error>> {
         unsafe { vgu::SetProcessDpiAwareness(2); }
-        let nwin = vgu::Window::new(title, (width as i32, height as i32), Some(global_winproc))?;
-        let rx = RenderContext::new(&nwin)?;
+        let mut d2fac = vgu::Factory::new()?;
+        let mut dpi: (f32, f32) = (0.0, 0.0);
+        unsafe { d2fac.GetDesktopDpi(&mut dpi.0, &mut dpi.1); }
+        let nwin = vgu::Window::new(title, (
+                ((width as f32) * (dpi.0 / 96.0)).ceil() as i32,
+                ((height as f32) * (dpi.1 / 96.0)).ceil() as i32), Some(global_winproc))?;
+        let mut rx = RenderContext::new(d2fac, &nwin)?;
         let mut win = Window {
             app, rx,
             nwin 
